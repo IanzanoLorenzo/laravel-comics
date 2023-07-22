4db3e06a16e1e5205eb765a36049367d120f77df
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-07-22 16:50:46 +0200

    inserito header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,48 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+<header>
+    <div class="header-overline">
+        <div class="container">
+            <div class="row align-items-center justify-content-end">
+                <div class="col-auto px-5">
+                    DC power&#8480;visa&#174;
+                </div>
+                <div class="col-auto">
+                    additional dc sites
+                </div>
+            </div>
+        </div>    
+    </div>
+    <nav>
+        <div class="container h-100">
+            <div class="row h-100">
+                <div class="col-auto">
+                    <img class="logo-h" src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="">
+                </div>
+                <div class="col">
+                    <div class="container-fluid">
+                        <div class="row align-items-center  justify-content-end">
+                            <div class="col-auto">
+                                <ul class="menu">
+                                    <li><a href="">character</a></li>
+                                    <li><a href="">comics</a></li>
+                                    <li><a href="">movies</a></li>
+                                    <li><a href="">tv</a></li>
+                                    <li><a href="">games</a></li>
+                                    <li><a href="">collectibles</a></li>
+                                    <li><a href="">videos</a></li>
+                                    <li><a href="">fans</a></li>
+                                    <li><a href="">news</a></li>
+                                    <li><a href="">shop</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-auto my-serchbar ms-5">
+                                <input type="text" class="my-input" placeholder="Search">
+                                <i class="fa-solid fa-magnifying-glass"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
